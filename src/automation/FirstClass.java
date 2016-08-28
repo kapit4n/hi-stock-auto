cc1108e40a65e1b496d5f3d654a804fe17053b70
llll <luis.arce22@gmail.com>
2016-08-27 21:54:04 -0400

    Updated the test case to edit configuration

--- src/automation/FirstClass.java
+++ src/automation/FirstClass.java
@@ -1,37 +1,55 @@
 package automation;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstClass {
 
+	static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
 
 		// Create a new instance of the Firefox driver
-		//String exePath = "D:\\projects\\aut-example\\chromedriver.exe";
+		// String exePath = "D:\\projects\\aut-example\\chromedriver.exe";
 		String exePath = "/home/llll/projects/hello-selenium/geckodriver";
 		System.setProperty("webdriver.gecko.driver", exePath);
-		//WebDriver driver = new ChromeDriver();
-		WebDriver driver = new FirefoxDriver();
+		// WebDriver driver = new ChromeDriver();
+		driver = new FirefoxDriver();
 
-        //Launch the Online Store Website
-		driver.get("http://www.store.demoqa.com");
+		driver.get("http://localhost:8081");
 
-        // Print a Log In message to the screen
-        System.out.println("Successfully opened the website www.Store.Demoqa.com");
-        Thread.sleep(10000);
+		// Wait for 5 Sec
+		Thread.sleep(3000);
+		login();
+		Thread.sleep(3000);
+		testCompanyShow();
+		Thread.sleep(3000);
+		testCompanyEdit();
+		Thread.sleep(3000);		
+		driver.quit();
+	}
+	
+	public static void login() throws InterruptedException {
+		System.out.println("Successfully opened the website www.Store.Demoqa.com");
+		Thread.sleep(5000);
+		driver.findElement(By.id("login")).sendKeys("admin");
+		driver.findElement(By.id("password")).sendKeys("admin");
+		Thread.sleep(2000);
+		driver.findElement(By.id("submitLogin")).click();;		
 
-        driver.findElement(By.cssSelector("a[href='http://store.demoqa.com/products-page/checkout/']")).click();
-        System.out.println(driver.findElement(By.cssSelector("a[href='http://store.demoqa.com/products-page/checkout/']")));
+	}
+	
+	public static void testCompanyShow() {
+		driver.findElement(By.id("company")).click();
+	}
 
-		//Wait for 5 Sec
-        Thread.sleep(10000);
-
-
-
-        // Close the driver
-        driver.quit();
-    }
-
+	public static void testCompanyEdit() throws InterruptedException {
+		driver.findElement(By.id("company_update")).click();
+		driver.findElement(By.id("companyName")).sendKeys("Dyamsoft");
+		driver.findElement(By.id("president")).sendKeys("Juan Salinas");
+		driver.findElement(By.id("description")).sendKeys("Dyamsoft is a sofwtare development company");
+		Thread.sleep(2000);
+		driver.findElement(By.id("updateSubmit")).click();;
+	}
 }
