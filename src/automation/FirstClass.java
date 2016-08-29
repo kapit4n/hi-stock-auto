38c62380106ad085ee3e876e931b0eec125a0dee
llll <luis.arce22@gmail.com>
2016-08-28 22:55:47 -0400

    updated the automation script to create measure, product, module and productor

--- src/automation/FirstClass.java
+++ src/automation/FirstClass.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class FirstClass {
 
@@ -27,6 +28,16 @@ public class FirstClass {
 		Thread.sleep(3000);
 		testCompanyEdit();
 		Thread.sleep(3000);		
+		testMeasureCreate();
+		Thread.sleep(3000);		
+		testProductCreate();
+		Thread.sleep(3000);		
+		testProveedorCreate();
+		Thread.sleep(3000);		
+		testModuleCreate();
+		Thread.sleep(3000);		
+		testProductorCreate();
+		Thread.sleep(3000);		
 		driver.quit();
 	}
 	
@@ -37,7 +48,6 @@ public class FirstClass {
 		driver.findElement(By.id("password")).sendKeys("admin");
 		Thread.sleep(2000);
 		driver.findElement(By.id("submitLogin")).click();;		
-
 	}
 	
 	public static void testCompanyShow() {
@@ -46,10 +56,93 @@ public class FirstClass {
 
 	public static void testCompanyEdit() throws InterruptedException {
 		driver.findElement(By.id("company_update")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.id("companyName")).sendKeys("Dyamsoft");
 		driver.findElement(By.id("president")).sendKeys("Juan Salinas");
 		driver.findElement(By.id("description")).sendKeys("Dyamsoft is a sofwtare development company");
 		Thread.sleep(2000);
 		driver.findElement(By.id("updateSubmit")).click();;
 	}
+
+	public static void testMeasureCreate() throws InterruptedException {
+		Actions action = new Actions(driver);
+		WebElement we = driver.findElement(By.id("measure"));
+		action.moveToElement(we).moveToElement(driver.findElement(By.id("measure_add"))).click().build().perform();
+		Thread.sleep(2000);
+		driver.findElement(By.id("name")).sendKeys("Measure Test");
+		driver.findElement(By.id("quantity")).sendKeys("1");
+		driver.findElement(By.id("description")).sendKeys("Measure Test");
+		driver.findElement(By.id("measureParent")).sendKeys("16");
+		Thread.sleep(2000);
+		driver.findElement(By.id("addSubmit")).click();;
+		System.out.println("Measure was created successsfully");
+	}
+	
+
+	public static void testProductCreate() throws InterruptedException {
+		Actions action = new Actions(driver);
+		WebElement we = driver.findElement(By.id("product"));
+		action.moveToElement(we).moveToElement(driver.findElement(By.id("product_add"))).click().build().perform();
+		Thread.sleep(2000);
+		driver.findElement(By.id("name")).sendKeys("Product Test");
+		driver.findElement(By.id("cost")).sendKeys("10");
+		driver.findElement(By.id("percent")).sendKeys("0.1");
+		driver.findElement(By.id("measureId")).sendKeys("1");
+		driver.findElement(By.id("currentAmount")).sendKeys("100");
+		driver.findElement(By.id("description")).sendKeys("This a product test");
+		driver.findElement(By.id("type")).sendKeys("1");
+		Thread.sleep(2000);
+		driver.findElement(By.id("addSubmit")).click();;
+		System.out.println("Product was created successsfully");
+	}	
+
+	public static void testProveedorCreate() throws InterruptedException {
+		Actions action = new Actions(driver);
+		WebElement we = driver.findElement(By.id("proveedor"));
+		action.moveToElement(we).moveToElement(driver.findElement(By.id("proveedor_add"))).click().build().perform();
+		Thread.sleep(2000);
+		driver.findElement(By.id("name")).sendKeys("Proveedor Test");
+		driver.findElement(By.id("phone")).sendKeys("60355282");
+		driver.findElement(By.id("address")).sendKeys("Simon Rodrigues");
+		driver.findElement(By.id("contact")).sendKeys("Juan Pablo");
+		driver.findElement(By.id("accountValue")).sendKeys("555");
+		Thread.sleep(2000);
+		driver.findElement(By.id("addSubmit")).click();;
+		System.out.println("Proveedor was created successsfully");
+	}
+
+	public static void testModuleCreate() throws InterruptedException {
+		Actions action = new Actions(driver);
+		WebElement we = driver.findElement(By.id("module"));
+		action.moveToElement(we).moveToElement(driver.findElement(By.id("module_add"))).click().build().perform();
+		Thread.sleep(2000);
+		driver.findElement(By.id("name")).sendKeys("Module Test");
+		driver.findElement(By.id("president")).sendKeys("Daniela Presidenta");
+		driver.findElement(By.id("description")).sendKeys("Module description");
+		driver.findElement(By.id("associationId")).sendKeys("1");
+		Thread.sleep(2000);
+		driver.findElement(By.id("addSubmit")).click();;
+		System.out.println("Module was created successsfully");
+	}
+
+	public static void testProductorCreate() throws InterruptedException {
+		Actions action = new Actions(driver);
+		WebElement we = driver.findElement(By.id("productor"));
+		action.moveToElement(we).moveToElement(driver.findElement(By.id("productor_add"))).click().build().perform();
+		Thread.sleep(2000);
+		driver.findElement(By.id("name")).sendKeys("Productor Test");
+		driver.findElement(By.id("ci")).sendKeys("5478745");
+		driver.findElement(By.id("phone")).sendKeys("485697");
+		driver.findElement(By.id("address")).sendKeys("Simon lopez 1212");
+		driver.findElement(By.id("accountId")).sendKeys("1616");
+		driver.findElement(By.id("moduleId")).sendKeys("1");
+		driver.findElement(By.id("acopio")).sendKeys("45");
+		driver.findElement(By.id("avg")).sendKeys("12");
+		driver.findElement(By.id("excedent")).sendKeys("10");
+		driver.findElement(By.id("pleno")).sendKeys("18");
+		Thread.sleep(2000);
+		driver.findElement(By.id("addSubmit")).click();;
+		System.out.println("Productor was created successsfully");
+	}
+	
 }
